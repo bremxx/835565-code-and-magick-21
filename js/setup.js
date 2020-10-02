16c21224975dbe676f74582cf66c077cd9a1dbc5
bremxx <petr.kirienko@gmail.com>
2020-10-02 13:52:18 +0300

    генерация и отображение похожих персонажей в блоке .setup

---
+++ js/setup.js
@@ -0,0 +1,81 @@
+"use strict";
+
+const SIMILAR_WIZARDS_NUM = 4;
+const setupWindow = document.querySelector(`.setup`);
+const setupSimilarBar = document.querySelector(`.setup-similar`);
+const similarWizardsList = document.querySelector(`.setup-similar-list`);
+const wizardTemplate = document
+  .querySelector(`#similar-wizard-template`)
+  .content.querySelector(`.setup-similar-item`);
+
+const firstNames = [
+  `Иван`,
+  `Хуан Себастьян`,
+  `Мария`,
+  `Кристоф`,
+  `Виктор`,
+  `Юлия`,
+  `Люпита`,
+  `Вашингтон`,
+];
+const lastNames = [
+  `да Марья`,
+  `Верон`,
+  `Мирабелла`,
+  `Вальц`,
+  `Онопко`,
+  `Топольницкая`,
+  `Нионго`,
+  `Ирвинг`,
+];
+const coatColors = [
+  `rgb(101, 137, 164)`,
+  `rgb(241, 43, 107)`,
+  `rgb(146, 100, 161)`,
+  `rgb(56, 159, 117)`,
+  `rgb(215, 210, 55)`,
+  `rgb(0, 0, 0)`,
+];
+const eyeColors = [`black`, `red`, `blue`, `yellow`, `green`];
+
+setupWindow.classList.remove(`hidden`);
+setupSimilarBar.classList.remove(`hidden`);
+
+function getRandomNumber(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+// Генерация волшебника
+function getRandomWizard() {
+  let randomWizard = {};
+  randomWizard.name = `${
+    firstNames[getRandomNumber(0, firstNames.length - 1)]
+  } ${lastNames[getRandomNumber(0, lastNames.length - 1)]}`;
+  randomWizard.eyesColor = eyeColors[getRandomNumber(0, eyeColors.length - 1)];
+  randomWizard.coatColor = coatColors[getRandomNumber(0, coatColors.length - 1)];
+
+  return randomWizard;
+}
+
+// Функция наполнения темплейта
+function insertWizard(obj) {
+  const newWizard = wizardTemplate.cloneNode(true);
+
+  newWizard.querySelector(`.setup-similar-label`).textContent = obj.name;
+  newWizard.querySelector(`.wizard-coat`).style.fill = obj.coatColor;
+  newWizard.querySelector(`.wizard-eyes`).style.fill = obj.eyesColor;
+
+  return newWizard;
+}
+
+// Наполнение блока похожих волшебников
+function insertSimilarWizards() {
+  const fragment = document.createDocumentFragment();
+  for (let i = 0; i < SIMILAR_WIZARDS_NUM; i++) {
+    fragment.appendChild(insertWizard(getRandomWizard()));
+  }
+
+  return similarWizardsList.appendChild(fragment);
+}
+
+insertSimilarWizards();
